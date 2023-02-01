ba3001ecc7ccfd742c0d7c3d075d9399d0ed3d6a
Amitkiit <116937662+Amitkiit@users.noreply.github.com>
2023-02-01 14:11:22 +0530

    Update router.js
    AWS Entry

--- src/routers/router.js
+++ src/routers/router.js
@@ -4,8 +4,57 @@ const {authentication} = require("../middlewares/commonMiddle")
 const {loginUser,createUser} = require("../controllers/userController")
 const {createBooks, getBook, updateBook, deleteBook,getBookById} = require("../controllers/bookController")
 const {createReview,deleteReview,reviewUpdate }= require("../controllers/reviewController")
+const aws= require("aws-sdk")
+//=======================AWS Entry========================================//
+aws.config.update({
+      accessKeyId: "AKIAY3L35MCRZNIRGT6N",
+      secretAccessKey: "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",
+      region: "ap-south-1"
+  })
 
-//user
+let uploadFile= async ( file) =>{
+   return new Promise( function(resolve, reject) {
+    // this function will upload file to aws and return the link
+    let s3= new aws.S3({apiVersion: '2006-03-01'}); // we will be using the s3 service of aws
+
+    var uploadParams= {
+        ACL: "public-read",
+        Bucket: "classroom-training-bucket",  //HERE
+        Key: "abc/" + file.originalname, //HERE 
+        Body: file.buffer
+    }
+    s3.upload( uploadParams, function (err, data ){
+        if(err) {
+            return reject({"error": err})
+        }
+        console.log(data)
+        console.log("file uploaded succesfully")
+        return resolve(data.Location)
+    })
+   })
+}
+
+router.post("/write-file-aws", async function(req, res){
+
+    try{
+        let files= req.files
+        if(files && files.length>0){
+            
+            let uploadedFileURL= await uploadFile( files[0] )
+            res.status(201).send({msg: "file uploaded succesfully", data: uploadedFileURL})
+        }
+        else{
+            res.status(400).send({ msg: "No file found" })
+        }
+        
+    }
+    catch(err){
+        res.status(500).send({msg: err})
+    }
+    
+})
+
+//============user>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.......
 router.post("/register", createUser)
 router.post("/login", loginUser)
 
